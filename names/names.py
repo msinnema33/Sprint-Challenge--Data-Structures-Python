1f28de11ae1ace28f5cf23ce22dac85ee7cf93cf
Mike Sinnema <msinnema33@gmail.com>
2020-06-12 08:27:25 -0700

    ran initial code and commented on initial runtime and complexity

--- names/names.py
+++ names/names.py
@@ -1,5 +1,7 @@
 import time
 
+from binary_search_tree import BinarySearchTree
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -17,10 +19,60 @@ for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
+# runtime before = 5.9769978523254395 seconds with 64 duplicates
+# complexity = O(n^2)
+
+
+# solution using a Binary Search Tree == MY TRY #1
+#
+# bst = BinarySearchTree('names')
+
+# for name in names_1:
+#     bst.insert(name)
+
+# for name in names_2:
+#     if bst.contains(name):
+#         duplicates.append(name)
+
+# runtime after = 0.10103750228881836 seconds
+# Complexity = O(nlog n)
+
+
+# stretch solution #1  -- STRETCH to show
+# # define a dictionary 
+# names_2_dict = {}
+
+# # Confirm names are in the list
+# for name in names_2:
+#     names_2_dict[name] = True
+# # Confirm name is in the list
+# # and if it is in the name 2 dict
+# # if so, append
+# for name in names_1:
+#     if name in names_2_dict:
+#         duplicates.append(name)
+
+# # runtime after = 0.007035255432128906 with 64 duplicates
+# # Complexity = Linear O(n)
+
+# using List comprehension MY TRY #2 ---Stretch
+# duplicates = [n1 for n1 in names_1 for n2 in names_2 if n1 == n2]
+    # run - time: 2.8079934120178223 seconds and 64 duplicates
+# duplicates = [n2 for n2 in names_2 for n1 in names_1 if n2 == n1]
+    # run - time:  2.479006052017212 seconds with 64 duplicates  
+
+
+# using set for both lists MY TRY #3 --Stretch
+# iterate through both and compare the two
+# & = compare. sets each bit to 1 if both bits are 1
+# duplicates = set(names_1) & set(names_2)  ## this is the line to uncomment
+    # run -time: 0.006998538970947266 seconds and 64 duplicates
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime was: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
